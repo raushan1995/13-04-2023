17077a46dc8d0deb9b90f3b30eddd515dac9df0a
Raushan Kumar <priyanshu1996r@gmail.com>
2023-04-13 22:47:14 +0530

    Add files via upload

---
+++ Reverse Of String/src/ReverseOfString.java
@@ -0,0 +1,25 @@
+
+
+//Java program to ReverseString using StringBuilder
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+//Class of ReverseString
+class ReverseOfString {
+	public static void main(String[] args)
+	{
+		String input = "raushan kumar";
+
+		StringBuilder input1 = new StringBuilder();
+
+		// append a string into StringBuilder input1
+		input1.append(input);
+
+		// reverse StringBuilder input1
+		input1.reverse();
+
+		// print reversed String
+		System.out.println(input1);
+	}
+}
